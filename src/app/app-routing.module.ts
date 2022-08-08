d2a5efe8a6be1731e44ee06ec3dc4eeb01413797
Luis0251 <luis0251@hotmail.com>
2022-08-08 16:31:17 -0500

    eliminar componente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DetailsComponent } from './components/details/details.component';
 import { SearchComponent } from './components/search/search.component';
 
 const routes: Routes = [
   {path:'**', component:SearchComponent},
-  {path:'detail',component:DetailsComponent}
 ];
 
 @NgModule({
